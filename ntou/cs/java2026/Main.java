646e8c02dbcb79e7a517a3e63485ac48908e6ad5
Shone <wilson9558@gmail.com>
2026-05-20 21:32:45 +0800

    新增使用者輸入功能

--- ntou/cs/java2026/Main.java
+++ ntou/cs/java2026/Main.java
@@ -4,12 +4,16 @@ import ntou.cs.java2026.crawler.BooksCrawler;
 import ntou.cs.java2026.crawler.PChomeCrawler;
 import ntou.cs.java2026.crawler.MomoCrawler;
 import ntou.cs.java2026.model.Product;
-import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String keyword = "Java";
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("=== 智慧購物比價追蹤器 ===");
+        System.out.print("請輸入搜尋關鍵字：");
+        String keyword = scanner.nextLine();
+
         System.out.println("搜尋關鍵字：" + keyword + "\n");
 
         // 博客來
@@ -30,5 +34,7 @@ public class Main {
         System.out.println("【momo】");
         MomoCrawler momo = new MomoCrawler();
         for (Product p : momo.search(keyword)) System.out.println(p);
+
+        scanner.close();
     }
 }
